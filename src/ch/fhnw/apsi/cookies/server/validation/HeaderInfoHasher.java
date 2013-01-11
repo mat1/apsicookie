b52f260fff9e29a6439b3fc94def7ee65cad22be
Florian Lüscher <florian@florianluescher.ch>
2013-01-11 17:23:36 +0100

    Add ignored

--- src/ch/fhnw/apsi/cookies/server/validation/HeaderInfoHasher.java
+++ src/ch/fhnw/apsi/cookies/server/validation/HeaderInfoHasher.java
@@ -1,11 +1,19 @@
 package ch.fhnw.apsi.cookies.server.validation;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import com.sun.net.httpserver.Headers;
 
 public class HeaderInfoHasher {
+	private static final Set<String> ignored = new HashSet<>();
+	static {
+		ignored.add("host");
+		ignored.add("cookie");
+	}
+	
 	public byte[] generateHeaderInfoHash(Headers headers) {
 		return concatenateHeader(headers).getBytes();
 	}
@@ -13,6 +21,9 @@ public class HeaderInfoHasher {
 	private String concatenateHeader(Headers headers) {
 		StringBuilder builder = new StringBuilder();
 		for(Entry<String, List<String>> e : headers.entrySet()) {
+			if(ignored.contains(e.getKey().toLowerCase()))
+				continue;
+			
 			builder.append(e.getKey());
 			appendList(builder, e.getValue());
 		}
